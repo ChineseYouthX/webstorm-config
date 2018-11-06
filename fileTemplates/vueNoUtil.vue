8202a7e8df47e0760c2070d9088917ee00e8e68c
Lurkerc <434848237@qq.com>
2018-11-06 20:54:01 +0800

    WS-2018.2.1 <lurke@DESKTOP-0NNM24N Create markdown.xml, apicloud-module-UIBarChart.xml, apicloud-module-UICityList.xml, apicloud-module-loadingLabel.xml, storeTemp.js, Default.xml, apicloud-module-downloadManager.xml, find.xml, apicloud-module-UISearchBar.xml, keymap.xml, apicloud-module-db.xml, apicloud-module-mdReader.xml, vue.vue, apicloud-module-aliPay.xml, Editplus copy.icls, React.xml, Default.xml, JavaScript.xml, apicloud-module-navigationBar.xml, apicloud-module-scrollRotation.xml, apicloud-module-actionButton.xml, apicloud-apijs.xml, apicloud-module-tabBar.xml, Zen XSL.xml, Handlebars.xml, apicloud-module-imageClip.xml, apicloud-module-coverFlow.xml, apicloud-module-UIListView.xml, editor.xml, apicloud-module-mcm.xml, apicloud-module-pieChart.xml, debugger.xml, apicloud-module-citySelector.xml, apicloud-module-imageFilter.xml, baseRefactoring.xml, apicloud-module-navigationMenu.xml, ide.general.xml, git.xml, _@user_Default.icls, CodeGlance.xml, apicloud-module-bookReader.xml, apicloud-module-inputField.xml, Zen CSS.xml, SCSS File.scss, ui.lnf.xml, apicloud-module-UIChatBox.xml, filetypes.xml, projectView.xml, apicloud-module-bMap.xml, plugin_ui.xml, apicloud-module-arcColorPicker.xml, html_xml.xml, notifications.xml, apicloud-module-cardReader.xml, Vue.xml, apicloud-module-slider.xml, apicloud-module-wxPay.xml, apicloud-module-UISelector.xml, apicloud-module-mam.xml, apicloud-module-panorama.xml, apicloud-module-UIGraph.xml, apicloud-module-pdfReader.xml, apicloud-module-fileBrowser.xml, material_custom_theme.xml, apicloud-module-imageBrowser.xml, Sass File.sass, apicloud-module-touchID.xml, apicloud-module-timeSelector.xml, web-browsers.xml, router.js, Less File.less, Vue Single File Component.vue, api.js, project.default.xml, apicloud-module-UICustomPicker.xml, apicloud-module-hintChatBox.xml, diff.xml, apicloud-module-socketManager.xml, apicloud-module-netAudio.xml, AngularJS .xml, _@user_Monokai.icls, file.template.settings.xml, ignore.xml, apicloud-module-pullMenu.xml, apicloud-module-sideMenu.xml, apicloud-module-speechRecognizer.xml, apicloud-module-polyVideo.xml, Dart.xml, Zen HTML.xml, apicloud-module-MNStack.xml, apicloud-module-shareAction.xml, apicloud-module-lineChart.xml, apicloud-module-FNScanner.xml, apicloud-module-push.xml, activate-power-mode_setting.xml, apicloud-module-fs.xml, vueNoUtil.vue, apicloud-module-UIInput.xml, apicloud.xml, apicloud-module-qq.xml, editor.codeinsight.xml, apicloud-module-UICalendar.xml, apicloud-module-bubbleMenu.xml, editor.rich.copy.xml, apicloud-module-jd.xml, apicloud-module-circularMenu.xml, apicloud-module-imageTool.xml, apicloud-module-personalCenter.xml, apicloud-module-arcProgress.xml, other.xml, apicloud-module-trans.xml, apicloud-module-bluetooth.xml, apicloud-module-periodSelector.xml, apicloud-module-docReader.xml, apicloud-module-tuberBar.xml, apicloud-module-wx.xml, apicloud-module-shakeView.xml, remote-servers.xml, xsl.xml, colors.scheme.xml, apicloud-module-listContact.xml, apicloud-module-UILinkedPicker.xml, Sublime Monokai True.icls, apicloud-module-arcMenu.xml, apicloud-module-clipBoard.xml, apicloud-module-contacts.xml, apicloud-module-button.xml, emmet.xml, apicloud-module-zip.xml, 2_sas_studentPc.xml, Angular.xml, apicloud-module-weibo.xml, laf.xml, apicloud-module-screenClip.xml, apicloud-module-UIScrollPicture.xml, apicloud-module-unionPay.xml, material_theme.xml, docker-registry.xml, apicloud-module-UIMediaScanner.xml, apicloud-module-tabBarMenu.xml, usageView.xml, apicloud-module-multiSelector.xml

---
+++ fileTemplates/vueNoUtil.vue
@@ -0,0 +1,34 @@
+<!--
+****--@file     ${NAME}
+****--@date     ${DATE} ${TIME}
+****--@author   YC
+****--@describe ${describe}
+-->
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+    /* 当前组件必要引入 */
+
+    // 当前组件引入全局的util
+    export default {
+        name: '${moduleName}',
+        data() {
+            return {}
+        },
+        methods: {
+            // 初始化请求列表数据
+            init() {
+            }
+
+        },
+        created() {
+            this.init();
+        },
+        mounted() {
+        },
+        components: {}
+    }
+</script>
